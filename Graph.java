81336cae44edc6c0d7564efef756342d1e9d7c34
Lw-Cui <645739460@QQ.com>
2015-12-18 22:49:54 +0800

    perfect class structure

--- Graph.java
+++ Graph.java
@@ -1,32 +1,67 @@
 import edu.princeton.cs.algs4.*;
 
 public class Graph {
-    public static void main(String[] args) {
+    private static class Point {
+        private double x, y;
+        private Bag<Point> adj;
 
-        int N = 1000;
-        int XMAX = 100, YMAX = 100;
-
-        int[] x = new int[N], y = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            x[i] = StdRandom.uniform(0, XMAX);
-            y[i] = StdRandom.uniform(0, YMAX);
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+           this.adj = new Bag<Point>();
         }
 
+        public void addEdge(Point v) {
+            adj.add(v);
+        }
+
+        public Iterable<Point> adj() {
+            return adj;
+        }
+
+        public void drawItself() {
+            StdDraw.filledCircle(x, y, 0.5);
+        }
+
+        public void drawTo(Point v) {
+            StdDraw.line(x, y, v.x, v.y);
+        }
+    }
+
+    final static private int XMAX = 100, YMAX = 100;
+    private Point[] points;
+    int V;
+
+    public Graph(int V, int E) {
+        this.V = V;
+        points = new Point[V];
+
+        for (int i = 0; i < V; i++)
+            points[i] = new Point(StdRandom.uniform(0.0, XMAX), StdRandom.uniform(0.0, YMAX));
+
+        for (int i = 0; i < E; i++) 
+            points[StdRandom.uniform(V)].addEdge(points[StdRandom.uniform(V)]);
+    }
+
+    public void draw() {
         StdDraw.setXscale(0, XMAX);
         StdDraw.setYscale(0, YMAX);
         StdDraw.setPenColor(StdDraw.RED);
         StdDraw.setPenRadius(0.0005);
 
-        for (int i = 0; i < N; i++) {
-            StdDraw.filledCircle(x[i], y[i], 0.5);
-
-            if (StdRandom.bernoulli(0.8) && i != 0) {
-                int nx = StdRandom.uniform(i);
-                //StdDraw.line(x[i], y[i], x[nx], y[nx]);
-                StdDraw.line(x[i], y[i], x[0], y[0]);
+        for (int i = 0; i < V; i++) {
+            points[i].drawItself();
+            for (Point v: points[i].adj()) {
+                v.drawItself();
+                points[i].drawTo(v);
             }
-            //StdDraw.show(500);
         }
+    }
+
+    public static void main(String[] args) {
+        int V = Integer.parseInt(args[0]);
+        int E = Integer.parseInt(args[1]);
+        Graph g = new Graph(V, E);
+        g.draw();
     }
 }
